4b5eca6fd079a2c0fcdf3ef4761a27a83b2df791
Ross <528266131@qq.com>
2022-05-23 20:35:07 +0800

    redux test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import AuthButton from "components/AuthButton";
+import User from "components/User";
 
 const App = () => {
   return (
     <div>
+      <section>
+        <User />
+      </section>
       <section>
         <AuthButton>登录</AuthButton>
       </section>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
 import "antd/dist/antd.css";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
